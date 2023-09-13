0176a6426d0f782ecc95c9e688cb28313487a443
BeatrizG <beatrizgarciaang0@gmail.com>
2023-09-13 16:12:12 -0300

    Se agrega menu responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,18 @@ const productSub = document.getElementById('product__submenu');
 const companySub = document.getElementById('company__submenu');
 const connectSub = document.getElementById('connect__submenu');
 
+const arrowProduct = document.getElementById('arrow_product');
+const arrowCompany = document.getElementById('arrow_company');
+const arrowConnect = document.getElementById('arrow_connect');
+
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('header-left');
+
 let productShow = false;
 let companyShow = false;
 let connectShow = false;
+
+let menuShow = false;
 
 btnProduct.addEventListener('click',()=>{
     if(!productShow){
@@ -16,10 +25,12 @@ btnProduct.addEventListener('click',()=>{
         companySub.style.display = "none";
         connectSub.style.display = "none";
         productShow = true;
+        arrowProduct.style.transform = 'rotate(180deg)';
     }
     else{
         productSub.style.display = "none";
         productShow = false;
+        arrowProduct.style.transform = 'rotate(0deg)';
     }
 })
 
@@ -29,10 +40,12 @@ btnCompany.addEventListener('click',()=>{
         productSub.style.display = "none";
         connectSub.style.display = "none";
         companyShow = true;
+        arrowCompany.style.transform = 'rotate(180deg)';
     }
     else{
         companySub.style.display = "none";
         companyShow = false;
+        arrowCompany.style.transform = 'rotate(0deg)';
     }
 })
 
@@ -42,11 +55,24 @@ btnConnect.addEventListener('click',()=>{
         productSub.style.display = "none";
         companySub.style.display = "none";
         connectShow = true;
+        arrowConnect.style.transform = 'rotate(180deg)';
     }
     else{
         connectSub.style.display = "none";
         connectShow = false;
+        arrowConnect.style.transform = 'rotate(0deg)';
     }
-})
+});
+
+btnMenu.addEventListener('click', ()=>{
+    if(!menuShow){
+        nav.style.display = "flex";
+        menuShow = true;
+    }
+    else{
+        nav.style.display = "none";
+        menuShow = false;
+    }
+});
 
 
